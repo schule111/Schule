b6aea7f2700624ef7c5c5396631687217fd83272
schule111 <shculemanagemnt@gmail.com>
2025-05-02 14:00:31 -0400

    Add files via upload

---
+++ shared/feature-cards.php
@@ -0,0 +1,86 @@
+<div class="container card-container"  id="feature-cards">
+        <div class="row g-5 show-cards">
+
+          <div class="col-12 col-md-4">
+            <div class="card border-0 shadow p-3 h-100">
+              <div class="py-3">
+                <h3 class="fs-4 justify-content-center">
+                  <span class="text-primary icon-hover"><i class='bx bxs-book'></i></span>
+                  Features
+                </h3>
+              </div>
+              <p>
+                A strong academic foundation is often key to achieving financial stability, with only a few exceptions. School also offers the chance to build meaningful friendships—some of which may last a lifetime.</p>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-4">
+            <div class="card border-0 shadow p-3 h-100">
+              <div class="py-3">
+                <h3 class="fs-4">
+                  <span class="text-primary icon-hover"><i class='bx bxs-star-half'></i></i></span>
+                  Achievement
+                </h3>
+              </div>
+              <p>
+                At the school awards, I was honored for my achievements in academic excellence, peer mentorship, environmental leadership, debate skills, and musical talent.</p>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-4">
+            <div class="card border-0 shadow p-3 h-100">
+              <div class="py-3">
+                <h3 class="fs-4">
+                  <span class="text-primary icon-hover"><i class='bx bxs-cuboid'></i></span>
+                  Goals
+                </h3>
+              </div>
+              <p>Fostering an environment where curiosity, collaboration, and innovation flourish, cultivating resilient leaders for the future.</p>
+              <p> Fostering a passion for learning.</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="row g-5 show-cards mt-3">
+
+          <div class="col-12 col-md-4">
+            <div class="card border-0 shadow p-3 h-100">
+              <div class="py-3">
+                <h3 class="fs-4 justify-content-center">
+                  <span class="text-primary icon-hover"><i class='bx bxs-bar-chart-alt-2'></i></span>
+                  Excellence
+                </h3>
+              </div>
+              <p>
+                Excellence is the result of dedication, perseverance, and a steadfast commitment to ongoing growth, establishing the benchmark for greatness in every pursuit.</p>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-4">
+            <div class="card border-0 shadow p-3 h-100">
+              <div class="py-3">
+                <h3 class="fs-4">
+                  <span class="text-primary icon-hover"><i class='bx bxs-user-badge'></i></span>
+                  Empowerment
+                </h3>
+              </div>
+              <p>
+                Empowerment is the key that unleashes the boundless potential within individuals, allowing them to harness their strengths and overcome challenges with confidence.</p>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-4">
+            <div class="card border-0 shadow p-3 h-100">
+              <div class="py-3">
+                <h3 class="fs-4">
+                  <span class="text-primary icon-hover"><i class='bx bxs-extension'></i></span>
+                  Creativity
+                </h3>
+              </div>
+              <p>Creativity is the dynamic force that brings ideas to life, lighting the way for new possibilities and shaping the world with endless innovation and imagination.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
